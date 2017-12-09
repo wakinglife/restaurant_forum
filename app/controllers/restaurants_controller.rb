8a5124190ef44e8c51de2d2fc5d75e2d4c873920
fion chen <wakinglife_615@yahoo.com>
2017-12-10 02:59:44 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,4 +5,8 @@ class RestaurantsController < ApplicationController
      @restaurants = Restaurant.page(params[:page]).per(9)
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
