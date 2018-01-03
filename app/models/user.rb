7011d86eef51722c077a92daa9f962e018302542
fion chen <wakinglife_615@yahoo.com>
2018-01-04 02:43:03 +0800

    create favorite model and setup relation among user,restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
    validates_presence_of :name, :email, :intro, :avatar
 
     mount_uploader :avatar, AvatarUploader
-    has_many :comments, dependent: :restrict_with_exception
+    has_many :comments, dependent: :restrict_with_error
     has_many :restaurants, through: :comments
 
+
+    has_many :favorites, dependent: :destroy
+    has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
     def admin?
        self.role == "admin"
