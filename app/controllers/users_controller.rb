b37f0b5c96cb918321f13b0f2c04c9c536e61abc
fion chen <wakinglife_615@yahoo.com>
2018-01-06 21:40:46 +0800

    add favorited restaurants, followings and followers to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   def show
 
     @commented_restaurants = @user.restaurants.uniq
+    @favorited_restaurants = @user.restaurants
+    @followings = @user.followings
+    @followers = @user.followers
   end
 
   def edit
